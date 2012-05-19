e45a6b7c6bf911eeaee7cc15e4739eb0d47f94b2
Steven Orvis <steven@trappingpa.com>
2012-05-18 21:56:58 -0400

    fixed program icon to boat from bluetooth chat program icon

--- gen/com/example/android/BluetoothChat/R.java
+++ gen/com/example/android/BluetoothChat/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int app_icon=0x7f020000;
+        public static final int app_icon_boat=0x7f020001;
     }
     public static final class id {
         public static final int BCURBar=0x7f060016;
